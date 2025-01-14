4c8969a2d0e6be00ffaf147fde760738ddabd258
mona <mona.halleem@gmail.com>
2025-01-14 19:30:33 +0200

    HTML5&CSS3 last assignment

--- lab 1/map/script.js
+++ lab 1/map/script.js
@@ -4,8 +4,12 @@ window.addEventListener('load',function(){
     let branchBtn = document.querySelector('#sideBranch');
     mainBtn.onclick=function(){
         branches[0].classList.toggle('highlight');
+        for(let i = 1; i<branches.length;i++){
+            branches[i].classList.remove('highlight');
+        }
     }
     branchBtn.onclick=function(){
+        branches[0].classList.remove('highlight');
         for(let i = 1; i<branches.length;i++){
             branches[i].classList.toggle('highlight');
         }
